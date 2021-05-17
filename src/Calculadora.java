1522c17e810e53ef6acc0670b988c2a58e39604e
NathanSFon <nathannaels276@gmail.com>
2021-05-17 11:16:49 -0300

    interface gráfica
    Adicionando a interface gráfica com a seleção de informações

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -3,11 +3,16 @@ import javax.swing.JOptionPane;
 import Operacoes.Acoes;
 
 public class Calculadora {
-
     public static void main(String[] args) {
         Acoes op = new Acoes();
-        //String a = JOptionPane.showInputDialog("Você deseja excluir este arquivo?");
-        int a = JOptionPane.showConfirmDialog(null, "Escolha a operação");
+        Object[] operacoes = {"Soma", "Subtração", "Multiplicação", "Divisão"};
+        Object selectedValue = JOptionPane.showInputDialog(null, "Escolha uma operação", "Opções", JOptionPane.INFORMATION_MESSAGE, null, operacoes, operacoes[0]);
+        String num00 =  JOptionPane.showInputDialog(null,"Digite um número");
+        String num01 =  JOptionPane.showInputDialog(null,"Digite um número");
+        
+        int resultado = op.soma(Integer.parseInt(num00), Integer.parseInt(num01));
+
+        JOptionPane.showMessageDialog(null, "O resultado é: " + resultado, "Resultado", JOptionPane.CLOSED_OPTION);
     }
     
 }
